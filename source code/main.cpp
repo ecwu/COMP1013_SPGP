d49c4c483dccf9b2e6480dcc5564c15fc8716d73
ecwu <bitlabwzh@gmail.com>
2017-12-13 17:50:52 +0800

    upload main function and students.txt

---
+++ source code/main.cpp
@@ -0,0 +1,15 @@
+/*
+	Structure Programming Group Project
+	Group Menber: Jack Wu, Irving Sau, Otto Zhang, Cheryl Shen.
+	Create Time: 2017-12-13
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+	char version[] = "0.0.1"; 
+	printf("Welcome to Score Management System %s\n", version);
+	return 0;
+}
